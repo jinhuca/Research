c29fb5194d44589bd9259c134956081810de62a0
Jin Hu <jinhuca@gmail.com>
2026-04-11 23:54:55 -0400

    33

---
+++ SystemInfo/QueryConstants/Win32VideoController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryConstants; 
+public static class Win32VideoController {
+  public const string QueryString = "SELECT * FROM Win32_VideoController";
+
+  public const string VideoProcessorKey = "VideoProcessor";
+  public const string VideoProcessorDesc = "Free-form string describing the video processor";
+}
---
+++ SystemInfo/SystemInfoCollection/WIn32VideoController.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SystemInfoCollection; 
+public static class WIn32VideoController {
+}
